3732ee9a9fa47a910381760424c34efafefeff39
Shekar <shekharsingh.singh3@gmail.com>
2021-08-30 08:57:23 +0530

    removed unwanted api

--- routes/web.php
+++ routes/web.php
@@ -15,32 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Route::get('api/v1/info','ExampleController@greeting');
-
-
-Route::post("api/v1/uploadss",function(Request $request){
-    $filename = $request->file('avatar')->store('', 'google');
-    $storage = Storage::disk("google");
-    $details = $storage->getMetadata($filename);
-    $url = $storage->url($filename);
-    dd($url);
-});
-
-
 Route::prefix('api/v1')->group(function () {
     Route::get('/info','ExampleController@greeting');
     Route::post('/upload', 'FileController@uploadFile');
-});
-
-
-Route::get('/getFile',function () {
-    $storage = Storage::disk("google");
-     $files =  $storage->allFiles();
-     $firstFileNmae=$files[0];
-    //  dd($firstFileNmae);
-     $details = $storage->getMetadata($firstFileNmae);
-    //  dd($details);
-     $url = $storage->url($firstFileNmae);
-     dd($url);
 });
